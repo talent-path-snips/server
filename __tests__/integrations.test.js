632e1d5fb0b1c81e973906053a3c95121bfef96a
Andrew P Maney <amaney@usc.edu>
2019-09-24 12:08:00 -0700

    Adds integration tests

---
+++ __tests__/integrations.test.js
@@ -0,0 +1,36 @@
+const request = require('supertest');
+const app = require('../app');
+const db = require('../db');
+const init = require('../db/init');
+
+beforeAll(async () => {
+  await init.createTables();
+  await init.seedAuthors();
+  await init.seedSnippets();
+});
+
+describe('snippets', () => {
+  afterEach(() => init.seedSnippets());
+
+  describe('GET snippets', () => {
+    // beforeEach(() => init.seedAuthors());
+    it('Gets all the snippets when passed no query parameters', async () => {
+      const response = await request(app).get('/api/snippets');
+      expect(response.body.length).toBe(2);
+    });
+
+    it('Gets some snippets when passed query parameters', async () => {
+      const response = await request(app)
+        .get('/api/snippets')
+        .query({ language: 'JavaScript' });
+      expect(response.body.length).toBe(1);
+      expect(response.body[0]).toMatchObject({
+        language: 'JavaScript',
+      });
+    });
+  });
+});
+
+afterAll(() => {
+  db.end();
+});
